a0c3691f6978d0d1ee7fe2d208024f8803efef44
baraahalomari <balomari995@gmail.com>
2021-08-08 13:41:31 +0300

    done

---
+++ src/components/footer/index.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+// export default Footer;
+
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer>&copy; 2018</footer>;
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/results/index.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+
+// class Results extends React.Component {
+//   render() {
+//     return (
+//       <section>
+//         <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+//       </section>
+//     );
+//   }
+// }
+
+// export default Results;
+
+import React from 'react'
+
+function Results(props) {
+  return (
+    <>
+      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+    </>
+  )
+}
+
+export default Results
+
