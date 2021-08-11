56d8b2274ad084d1688ca5f367f8a1bb908875ec
baraahalomari <balomari995@gmail.com>
2021-08-11 17:08:03 +0300

    lab-28

--- src/components/results/results.test.js
+++ src/components/results/results.test.js
@@ -1,3 +1,23 @@
+// import {render, screen} from '@testing-library/react';
+// import Result from './index';
+// import '@testing-library/jest-dom/extend-expect';
+
+// it('Should render results', () => {
+//     const data = {
+//         Headers:{
+//         'content-type': 'string application/json',
+//         },
+//         count: 2,
+//         results: [
+//             {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+//             {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+//         ],
+//     };
+//     render(<Result data={data} />);
+//     const items = screen.getByTestId('results');
+//     expect(items).toHaveTextContent('{ "Headers": { "content-type": "string application/json" }, "count": 2, "results": [ { "name": "fake thing 1", "url": "http://fakethings.com/1" }, { "name": "fake thing 2", "url": "http://fakethings.com/2" } ] }');
+// });
+
 import {render, screen} from '@testing-library/react';
 import Result from './index';
 import '@testing-library/jest-dom/extend-expect';
@@ -15,5 +35,5 @@ it('Should render results', () => {
     };
     render(<Result data={data} />);
     const items = screen.getByTestId('results');
-    expect(items).toHaveTextContent('{ "Headers": { "content-type": "string application/json" }, "count": 2, "results": [ { "name": "fake thing 1", "url": "http://fakethings.com/1" }, { "name": "fake thing 2", "url": "http://fakethings.com/2" } ] }');
+    expect(items).toHaveTextContent('[ { "name": "fake thing 1", "url": "http://fakethings.com/1" }, { "name": "fake thing 2", "url": "http://fakethings.com/2" } ]');
 });
