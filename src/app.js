d125532e118fea956232c0cdf7dab6d442d45188
baraahalomari <balomari995@gmail.com>
2021-08-11 21:25:46 +0300

    lab29

--- src/app.js
+++ src/app.js
@@ -54,10 +54,9 @@
 // export default App;
 
 
-
 // import React from 'react';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect,useReducer } from 'react';
 import './app.scss';
 
 // Let's talk about using index.js and some other name in the component folder
@@ -66,8 +65,35 @@ import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results';
+import History from './components/history/history';
+
+const initialState=[];
+
+function reducer(history=initialState,action){
+  const {type,payload}=action;
+  switch (type) {
+    case 'AddToHistory':
+      console.log(history);
+      history=[...history,payload];
+      return history;
+    default:
+      return history;
+  }
+}
+function addToHistory(url,method,result){
+
+  return({
+    type:'AddToHistory',
+    payload:{
+      url,
+      method,
+      result
+    }
+  })
+}
 
 function App() {
+  const [history, dispatch] = useReducer(reducer, initialState)
   const [data, setData] = useState(null);
   const [requestParams, setRequestParams] = useState({});
   const [requestBody, setRequestBody] = useState({});
@@ -79,10 +105,13 @@ function App() {
       const result = await axios[requestParams.method](requestParams.url, JSON.parse(requestBody));
       const data = { headers: result.headers, count: result.data.count, results: result.data.results }
       setData(data);
+      dispatch(addToHistory(requestParams.url,requestParams.method,data));
     } else {
       const result = await axios[requestParams.method](requestParams.url);
       const data = { headers: result.headers, count: result.data.count, results: result.data.results }
       setData(data);
+      dispatch(addToHistory(requestParams.url,requestParams.method,data));
+ 
     }
   }, [requestParams]);
 
@@ -91,12 +120,16 @@ function App() {
     setRequestParams(requestParams);
     setRequestBody(requestBody);
   }
+  function historyfunc(result){
+    setData(result);
+  }
   return (
     <>
       <Header />
       <div>Request Method: {requestParams.method}</div>
       <div>URL: {requestParams.url}</div>
       <Form handleApiCall={callApi} />
+      {history&&<History historyfunc={historyfunc} history={history} />}
       <Results data={data} />
       <Footer />
     </>
